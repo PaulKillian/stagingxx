964d4201fac94d7d2f173d0f2ee229b6aece8047
Paul_Killian <psk65lava@gmail.com>
2021-06-02 17:13:18 -0700

    label

--- public_html/js/form.js
+++ public_html/js/form.js
@@ -10,50 +10,6 @@ const main = document.getElementById('main');
 
 const form = document.forms[0];
 
-// form.addEventListener("submit", function(event) {
-  // const url = storageRef.getDownloadURL()ß
-  // event.preventDefault();
-  // const formData = new FormData(this);
-  // const entries = formData.entries();
-  // const data = Object.fromEntries(entries)
-  
-  // sendFormData()
-  // Email.send({
-  //   Host : "aspmx.l.google.com",
-  //   Username : "psk65lava@gmail.com",
-  //   Password : "Sammy6565656565",
-  //   To : 'psk65lava@gmail.com',
-  //   From : "psk65lava@gmail.com",
-  //   Subject : "This is the subject",
-  //   Body : "And this is the body",
-  //   Attachments: [{
-  //     name: "1622472894384.pdf",
-  //     path: "https://firebasestorage.googleapis.com/v0/b/xx-artists.appspot.com/o/1622472894384?alt=media&token=d2df0c9f-924a-4b83-b763-4cbc2d47f79a"
-  //     }]
-  // }).then(
-  // message => alert(message)
-  // );
-// }); 
-
-// const sendFormData = () => {
-//   Email.send({
-//     Host : "aspmx.l.google.com",
-//     Username : "psk65lava@gmail.com",
-//     Password : "Sammy6565656565",
-//     To : 'psk65lava@gmail.com',
-//     From : "psk65lava@gmail.com",
-//     Subject : "This is the subject",
-//     Body : "And this is the body",
-//     Attachments: [{
-//       name: "1622472894384",
-//       path: "gs://xx-artists.appspot.com/1622472894384"
-//       }]
-//   }).then(
-//   message => alert(message)
-//   );
-
-// }
-
 const clearAndSubmission = () => {
   setTimeout(function() {
   main.innerHTML = `
@@ -104,27 +60,17 @@ const submitForm = () => {
 
 const endUpload = (label, file) => {
   setTimeout(function() {
-    // element.classList.remove('loader')
     clearTimeout(uploading)
+    label.parentElement.classList.add('background')
     label.innerText = file
   }, 2000);
 }
 
 function fileNameReplace(event){
-//   $(document).on("change", ".inputfile[type=file]", function () {
-//     var file = this.files[0];
-//     if (file.size > 5000) {
-//         alert("Too large Image. Only image smaller than 2MB can be uploaded.");
-//         $(this).replaceWith('<input type="file" name="my_file" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" onchange="fileNameReplace(event)" />');
-//         return false;
-//     }
-// });
   const file_data = $('#file-1').prop('files')[0];
   const storageRef = firebase.storage().ref();
   const resumesRef = storageRef.child(`resumes/${file_data.name}`);
   resumesRef.put(file_data);
-
- 
   
   const id = event.target.id
   const name = document.getElementById(id); 
@@ -133,9 +79,6 @@ function fileNameReplace(event){
   const newLabel = label.nextSibling.childNodes[0].id
   const spanLabel = document.getElementById(newLabel)
   uploading = setTimeout(function() {
-    // const uploadEl = document.getElementById(id);
-    // uploadEl.classList.add('loader')
-    // uploadEl.innerText="uploading"
     spanLabel.innerText = "uploading..."
     endUpload(spanLabel, fileName);
     }, 500);
